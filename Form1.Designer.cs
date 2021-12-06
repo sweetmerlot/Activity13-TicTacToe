282fd8368d9e4e8293cb921306c462880e7344ce
Jennifer <jnarvaez07@gmail.com>
2021-12-05 23:01:12 -0600

    Initial Commit Activity 13, CST-150, Tic Tac Toe

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Activity13XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xo00 = new System.Windows.Forms.Label();
+            this.xo01 = new System.Windows.Forms.Label();
+            this.xo02 = new System.Windows.Forms.Label();
+            this.xo12 = new System.Windows.Forms.Label();
+            this.xo11 = new System.Windows.Forms.Label();
+            this.xo10 = new System.Windows.Forms.Label();
+            this.xo22 = new System.Windows.Forms.Label();
+            this.xo21 = new System.Windows.Forms.Label();
+            this.xo20 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // xo00
+            // 
+            this.xo00.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo00.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo00.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo00.ForeColor = System.Drawing.Color.Purple;
+            this.xo00.Location = new System.Drawing.Point(22, 35);
+            this.xo00.Name = "xo00";
+            this.xo00.Size = new System.Drawing.Size(83, 62);
+            this.xo00.TabIndex = 0;
+            this.xo00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo00.Click += new System.EventHandler(this.xo00_Click);
+            // 
+            // xo01
+            // 
+            this.xo01.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo01.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo01.ForeColor = System.Drawing.Color.Purple;
+            this.xo01.Location = new System.Drawing.Point(111, 35);
+            this.xo01.Name = "xo01";
+            this.xo01.Size = new System.Drawing.Size(83, 62);
+            this.xo01.TabIndex = 1;
+            this.xo01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo01.Click += new System.EventHandler(this.xo01_Click);
+            // 
+            // xo02
+            // 
+            this.xo02.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo02.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo02.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.xo02.Location = new System.Drawing.Point(200, 35);
+            this.xo02.Name = "xo02";
+            this.xo02.Size = new System.Drawing.Size(83, 62);
+            this.xo02.TabIndex = 2;
+            this.xo02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo02.Click += new System.EventHandler(this.xo02_Click);
+            // 
+            // xo12
+            // 
+            this.xo12.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo12.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo12.Location = new System.Drawing.Point(200, 104);
+            this.xo12.Name = "xo12";
+            this.xo12.Size = new System.Drawing.Size(83, 62);
+            this.xo12.TabIndex = 5;
+            this.xo12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo12.Click += new System.EventHandler(this.xo12_Click);
+            // 
+            // xo11
+            // 
+            this.xo11.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo11.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo11.Location = new System.Drawing.Point(111, 104);
+            this.xo11.Name = "xo11";
+            this.xo11.Size = new System.Drawing.Size(83, 62);
+            this.xo11.TabIndex = 4;
+            this.xo11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo11.Click += new System.EventHandler(this.xo11_Click);
+            // 
+            // xo10
+            // 
+            this.xo10.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo10.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo10.Location = new System.Drawing.Point(22, 104);
+            this.xo10.Name = "xo10";
+            this.xo10.Size = new System.Drawing.Size(83, 62);
+            this.xo10.TabIndex = 3;
+            this.xo10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo10.Click += new System.EventHandler(this.xo10_Click);
+            // 
+            // xo22
+            // 
+            this.xo22.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo22.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo22.Location = new System.Drawing.Point(200, 173);
+            this.xo22.Name = "xo22";
+            this.xo22.Size = new System.Drawing.Size(83, 62);
+            this.xo22.TabIndex = 8;
+            this.xo22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo22.Click += new System.EventHandler(this.xo22_Click);
+            // 
+            // xo21
+            // 
+            this.xo21.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo21.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo21.Location = new System.Drawing.Point(111, 173);
+            this.xo21.Name = "xo21";
+            this.xo21.Size = new System.Drawing.Size(83, 62);
+            this.xo21.TabIndex = 7;
+            this.xo21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo21.Click += new System.EventHandler(this.xo21_Click);
+            // 
+            // xo20
+            // 
+            this.xo20.BackColor = System.Drawing.SystemColors.Menu;
+            this.xo20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.xo20.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xo20.Location = new System.Drawing.Point(22, 173);
+            this.xo20.Name = "xo20";
+            this.xo20.Size = new System.Drawing.Size(83, 62);
+            this.xo20.TabIndex = 6;
+            this.xo20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.xo20.Click += new System.EventHandler(this.xo20_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Thistle;
+            this.groupBox1.Controls.Add(this.xo02);
+            this.groupBox1.Controls.Add(this.xo22);
+            this.groupBox1.Controls.Add(this.xo00);
+            this.groupBox1.Controls.Add(this.xo21);
+            this.groupBox1.Controls.Add(this.xo01);
+            this.groupBox1.Controls.Add(this.xo20);
+            this.groupBox1.Controls.Add(this.xo10);
+            this.groupBox1.Controls.Add(this.xo12);
+            this.groupBox1.Controls.Add(this.xo11);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 267);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Tag = "xo";
+            this.groupBox1.Text = "X Goes First!";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 40);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(334, 379);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label xo00;
+        private System.Windows.Forms.Label xo01;
+        private System.Windows.Forms.Label xo02;
+        private System.Windows.Forms.Label xo12;
+        private System.Windows.Forms.Label xo11;
+        private System.Windows.Forms.Label xo10;
+        private System.Windows.Forms.Label xo22;
+        private System.Windows.Forms.Label xo21;
+        private System.Windows.Forms.Label xo20;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity13XO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] gameBoard = new string[3, 3];
+
+        bool isWinner = false;
+        //button clicks
+        int turn = -1;
+        
+       
+        private void xo00_Click(object sender, EventArgs e)
+        {
+            if(xo00.Text == "")
+            {
+                turn *= -1;
+                                
+                if (turn > 0)
+                {
+                    xo00.Text = "X";
+                    xo00.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo00.Text = "O";
+                    xo00.ForeColor = Color.MediumPurple;
+                }                                
+                this.Refresh();
+
+                gameBoard[0, 0] = xo00.Text;
+
+                Winner(gameBoard);
+
+                if(isWinner == true)
+                {
+                    if(turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }                        
+        }
+
+        private void xo01_Click(object sender, EventArgs e)
+        {
+            if (xo01.Text == "")
+            {
+                int nextTurn = turn * -1;
+                if (nextTurn > 0)
+                {
+                    xo01.Text = "X";
+                    xo01.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo01.Text = "O";
+                    xo01.ForeColor = Color.MediumPurple;
+                }
+                turn = nextTurn;
+                this.Refresh();
+                gameBoard[0, 1] = xo01.Text;
+
+                Winner(gameBoard);
+                if (isWinner == true)
+                {
+                    if (turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }
+        }
+
+        private void xo02_Click(object sender, EventArgs e)
+        {
+            if (xo02.Text == "")
+            {
+                int nextTurn = turn * -1;
+                if (nextTurn > 0)
+                {
+                    xo02.Text = "X";
+                    xo02.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo02.Text = "O";
+                    xo02.ForeColor = Color.MediumPurple;
+                }
+                turn = nextTurn;
+                this.Refresh();
+                gameBoard[0, 2] = xo02.Text;
+
+                Winner(gameBoard);
+                if (isWinner == true)
+                {
+                    if (turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }
+        }
+
+        private void xo10_Click(object sender, EventArgs e)
+        {
+            if (xo10.Text == "")
+            {
+                int nextTurn = turn * -1;
+                if (nextTurn > 0)
+                {
+                    xo10.Text = "X";
+                    xo10.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo10.Text = "O";
+                    xo10.ForeColor = Color.MediumPurple;
+                }
+                turn = nextTurn;
+                this.Refresh();
+
+                gameBoard[1, 0] = xo10.Text;
+
+                Winner(gameBoard);
+                if (isWinner == true)
+                {
+                    if (turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }
+        }
+
+        private void xo11_Click(object sender, EventArgs e)
+        {
+            if (xo11.Text == "")
+            {
+                int nextTurn = turn * -1;
+                if (nextTurn > 0)
+                {
+                    xo11.Text = "X";
+                    xo11.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo11.Text = "O";
+                    xo11.ForeColor = Color.MediumPurple;
+                }
+                turn = nextTurn;
+                this.Refresh();
+
+                gameBoard[1, 1] = xo11.Text;
+
+                Winner(gameBoard);
+                if (isWinner == true)
+                {
+                    if (turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }
+        }
+
+        private void xo12_Click(object sender, EventArgs e)
+        {
+            if (xo12.Text == "")
+            {
+                int nextTurn = turn * -1;
+                if (nextTurn > 0)
+                {
+                    xo12.Text = "X";
+                    xo12.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo12.Text = "O";
+                    xo12.ForeColor = Color.MediumPurple;
+                }
+                turn = nextTurn;
+                this.Refresh();
+
+                gameBoard[1, 2] = xo12.Text;
+
+                Winner(gameBoard);
+                if (isWinner == true)
+                {
+                    if (turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }
+        }
+
+        private void xo20_Click(object sender, EventArgs e)
+        {
+            if (xo20.Text == "")
+            {
+                int nextTurn = turn * -1;
+                if (nextTurn > 0)
+                {
+                    xo20.Text = "X";
+                    xo20.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo20.Text = "O";
+                    xo20.ForeColor = Color.MediumPurple;
+                }
+                turn = nextTurn;
+                this.Refresh();
+
+                gameBoard[2, 0] = xo20.Text;
+
+                Winner(gameBoard);
+                if (isWinner == true)
+                {
+                    if (turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }
+        }
+
+        private void xo21_Click(object sender, EventArgs e)
+        {
+            if (xo21.Text == "")
+            {
+                int nextTurn = turn * -1;
+                if (nextTurn > 0)
+                {
+                    xo21.Text = "X";
+                    xo21.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo21.Text = "O";
+                    xo21.ForeColor = Color.MediumPurple;
+                }
+                turn = nextTurn;
+                this.Refresh();
+                gameBoard[2, 1] = xo21.Text;
+
+                Winner(gameBoard);
+                if (isWinner == true)
+                {
+                    if (turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }
+        }
+
+        private void xo22_Click(object sender, EventArgs e)
+        {
+            if (xo22.Text == "")
+            {
+                int nextTurn = turn * -1;
+                if (nextTurn > 0)
+                {
+                    xo22.Text = "X";
+                    xo22.ForeColor = Color.Purple;
+                }
+                else
+                {
+                    xo22.Text = "O";
+                    xo22.ForeColor = Color.MediumPurple;
+                }
+                turn = nextTurn;
+                this.Refresh();
+                gameBoard[2, 2] = xo22.Text;
+
+                Winner(gameBoard);
+                if (isWinner == true)
+                {
+                    if (turn > 0)
+                    {
+                        MessageBox.Show("X Wins");
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Wins!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is already a value here. Try again");
+            }
+        }
+                             
+       private bool Winner(string[,] board)
+        {
+            //winner - check each row
+            for(int row = 0; row < 3; row++)
+            {
+                if(gameBoard[row,0] == gameBoard[row,1] && gameBoard[row, 1] == gameBoard[row, 2])
+                {
+                    if(gameBoard[row,0] != null && gameBoard[row, 1] != null && gameBoard[row, 2] != null)
+                    {
+                        isWinner = true;
+                        groupBox1.Enabled = false;
+                    }                   
+                }
+            }
+            //winner - check each col
+            for (int col = 0; col < 3; col++)
+            {
+                if (gameBoard[0, col] == gameBoard[1, col] && gameBoard[1, col] == gameBoard[2, col])
+                {
+                    if (gameBoard[0, col] != null && gameBoard[1, col] != null && gameBoard[2, col] != null)
+                    {
+                        isWinner = true;
+                        groupBox1.Enabled = false;
+                    }
+                    
+                }
+            }
+            //winner - check right diagonal
+            if(gameBoard[0, 0] == gameBoard[1, 1] && gameBoard[1, 1] == gameBoard[2, 2])
+            {
+                if(gameBoard[1,1] != null)
+                {
+                    isWinner = true;
+                    groupBox1.Enabled = false;
+                }
+                
+            }
+            //winner - check left diagonal
+            if (gameBoard[2,0] == gameBoard[1, 1] && gameBoard[1, 1] == gameBoard[0,2])
+            {
+                if (gameBoard[1, 1] != null)
+                {
+                    isWinner = true;
+                    groupBox1.Enabled = false;
+                }
+            }
+
+            return isWinner;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+          
+        }
+    }
+    
+}
